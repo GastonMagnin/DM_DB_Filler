755e38c2f6e727277be12c5756e2714911992362
GastonMagnin <44193118+GastonMagnin@users.noreply.github.com>
2019-11-27 12:27:05 +0100

    cleaned up code and added some more comments

--- InsertWorkerThread.py
+++ InsertWorkerThread.py
@@ -36,11 +36,18 @@ class InsertWorkerThread(QThread):
         self.amount = amount
 
     def insert_data(self):
+        """
+        Takes lists of columns and the values entered for these columns, checks for special inputs like random(getuserdata) and
+        provides the data needed to replace them, then generates {amount} sql statements using the inputs and executes them
+        Sends a signal to the main ui thread when it's done containing the number of successful inserts
+        :return:
+        """
+        # initialize lists
         keywords = get_keywords()
         amount = self.amount
         table_name = self.table_name
         vals = self.vals
-        rows = self.rows
+        columns = self.rows
         data = []
         user_data = []
         succ_counter = 0
@@ -68,9 +75,9 @@ class InsertWorkerThread(QThread):
                     # self.connection_bar.setValue(0)
                     try:
                         if user_data:
-                            cursor.execute(self.insert_factory(table_name, vals, rows, data, user_data[i]))
+                            cursor.execute(self.insert_factory(table_name, vals, columns, data, user_data[i]))
                         else:
-                            cursor.execute(self.insert_factory(table_name, vals, rows, data))
+                            cursor.execute(self.insert_factory(table_name, vals, columns, data))
                         succ_counter += 1
                     except Exception as e:
                         print(e)
@@ -89,40 +96,64 @@ class InsertWorkerThread(QThread):
         self.inserts_done.emit(succ_counter)
 
 
-    def insert_factory(self, table_name, vals, rows, data=None, user_data=None):
-        print(data)
+    def insert_factory(self, table_name, vals, columns, data=None, user_data=None):
+        """
+        Builds a SQL Insert String using the passed data, replaces special inputs like random(getuserdata) with the
+        correct replacement
+        :param table_name: the name of the table into which the data is to be inserted
+        :param vals: the values (may contain special inputs) to be written to the table(after special inputs are replaced)
+        :param columns: the columns into which the values are to be inserted
+        :param data: (optional) if there are special inputs containing a table and columnname the contents of these columns will be passed here
+        :param user_data: (optional) if there are special inputs in the form of random(getuserdata) a dict containing data for (almost) every column will be passed here
+        :return: SQL String built with the passed data
+        """
         counter = 0
         newvals = []
+        # check for special inputs and replace them
         for i in range(len(vals)):
+            # check for random(getuserdata)
             if re.search("^random\(getuserdata\)$", vals[i]) is not None:
-                newvals.append(user_data[rows[i]])
+                newvals.append(user_data[columns[i]])
+            # check for random(gibberish)
             elif re.search("^random\((gibberish)\)", vals[i]) is not None:
                 newvals.append(self.get_gibberish())
+            # check for random(rand_number[])
             elif re.search("^random\(rand_number\[([0-9]*)[,-:]([0-9]*)\]\)", vals[i]) is not None:
                 x = re.search("^random\(rand_number\[([0-9]*)[,-:]([0-9]*)\]\)", vals[i])
                 newvals.append(str(random.randint(int(x.group(1)), int(x.group(2)))))
+            # check for random(tablename.columnname)
             elif re.search("^random\((.*)\)", vals[i]) is not None:
                 # [0] cursor fetchall returns tuples
                 newvals.append(str(data[counter][random.randint(0, len(data[counter]) - 1)][0]))
                 counter += 1
             else:
+                # if the line has no special inputs use the line itself
                 newvals.append(vals[i])
+        # build sql string
         sql = "INSERT INTO %s%s VALUES (%s)" % (
-            table_name, "" if not rows else "(" + (",".join(rows)) + ")", ", ".join("\"%s\"" % x for x in newvals))
+            table_name, "" if not columns else "(" + (",".join(columns)) + ")", ", ".join("\"%s\"" % x for x in newvals))
         print(sql)
         return sql
 
     # noinspection SpellCheckingInspection
     def get_random_user_data(self):
+        """
+        gets random user data from an api and builds a dict with relevant values
+        :return:
+        """
+        # initialize output
         output = {}
+        # build request
         url = "https://randomuser.me/api"
         req = urllib.request.Request(url)
+        # get json from api and convert it
         try:
             with urllib.request.urlopen(req) as data:
                 user_data = json.loads(data.read())["results"][0]
         except Exception as e:
             print(e.with_traceback())
             return
+        # build output dict from json
         output["handle"] = user_data["login"]["username"][0:15]
         output["anzeigename"] = " ".join(list(user_data["name"].values())[1:])
         output["email"] = user_data["email"]
@@ -136,26 +167,24 @@ class InsertWorkerThread(QThread):
         output["geburtsdatum_monat_und_tag_sichtbarkeit"] = str(random.randint(1, 5))
         output["ort"] = user_data["location"]["city"]
         # get gibberish text
-        url = "https://www.randomtext.me/api/gibberish/p-1/15-25"
-        req = urllib.request.Request(url, headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"})
-        try:
-            with urllib.request.urlopen(req) as x:
-                xdict = json.loads(x.read())
-        except Exception as e:
-            print(e)
-            return
-        output["bio"] = xdict["text_out"][3:-5]
+        output["bio"] = self.get_gibberish()
         return output
 
     def get_gibberish(self):
+        """
+        Gets gibberish text from an api
+        :return: String containing gibberish text
+        """
+        # build request
         url = "https://www.randomtext.me/api/gibberish/p-1/15-25"
         req = urllib.request.Request(url, headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"})
+        # get json from api
         try:
             with urllib.request.urlopen(req) as x:
                 xdict = json.loads(x.read())
         except Exception as e:
             print(e.with_traceback())
             return
+        # extract the text
         return xdict["text_out"][3:-5]
